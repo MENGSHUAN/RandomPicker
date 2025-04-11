42c799880c21ba896976e4fb34da97adef8e6a49
MENGSHUAN <mark102555@gmail.com>
2025-04-11 19:19:00 +0800

    delete database

--- routes/web.php
+++ routes/web.php
@@ -7,10 +7,4 @@ use App\Http\Controllers\OptionController;
 
 Route::get('/wheel', [WheelController::class, 'wheel']);
 Route::post('/draw', [WheelController::class, 'draw']);
-Route::get('/slot', [SlotController::class, 'slot']);
-Route::post('/slot/draw', [SlotController::class, 'draw']);
-Route::post('/slot/reset', [SlotController::class, 'resetOptions'])->name('slot.reset');
-Route::get('/options', [OptionController::class, 'view'])->name('options.view');
-Route::post('/options', [OptionController::class, 'store'])->name('options.store');
-Route::put('/options/{option}', [OptionController::class, 'update'])->name('options.update');
-Route::delete('/options/{option}', [OptionController::class, 'destroy'])->name('options.destroy');
+
--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -18,22 +18,4 @@ class ExampleTest extends TestCase
         $response = $this->get('/wheel');
         $response->assertStatus(200);
     }
-
-    /**
-     * 測試老虎機遊戲頁面是否可以正常訪問
-     */
-    public function test_slot_page_returns_successful_response(): void
-    {
-        $response = $this->get('/slot');
-        $response->assertStatus(200);
-    }
-
-    /**
-     * 測試選項管理頁面是否可以正常訪問
-     */
-    public function test_options_page_returns_successful_response(): void
-    {
-        $response = $this->get('/options');
-        $response->assertStatus(200);
-    }
 }
