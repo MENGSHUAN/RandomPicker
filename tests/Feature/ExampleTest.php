56fb57c157df6a351719130fcfd5ad1680720737
MENGSHUAN <mark102555@gmail.com>
2025-05-04 12:38:11 +0800

    Fix heroku deployment

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,20 +2,17 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
-    use RefreshDatabase;
-
-
     /**
-     * 測試輪盤遊戲頁面是否可以正常訪問
+     * A basic test example.
      */
-    public function test_wheel_page_returns_successful_response(): void
+    public function test_the_application_returns_a_successful_response(): void
     {
         $response = $this->get('/');
+
         $response->assertStatus(200);
     }
 }
