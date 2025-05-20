9ccd75b5247a38251511b02f8241a22824b8a2c8
MENGSHUAN <mark102555@gmail.com>
2025-05-21 02:01:29 +0800

    modify wheel.blade.php for demo

--- resources/views/wheel.blade.php
+++ resources/views/wheel.blade.php
@@ -1,3 +1,6 @@
+{{-- {{ 故意讓 blade 出錯 }}
+{{ $this_variable_is_not_defined->foo }} --}}
+
 <!DOCTYPE html>
 <html lang="zh-TW">
 <head>
@@ -14,7 +17,7 @@
             justify-content: center;
             min-height: 100vh;
             margin: 0;
-            background-color:rgb(149, 150, 194);
+            background-color:rgb(238, 238, 247);
             font-family: Arial, sans-serif;
         }
 
@@ -190,9 +193,9 @@
     </style>
 </head>
 <body>
-    <div style="position: absolute; top: 30px; left: 0; width: 100%; text-align: center; z-index: 10; font-size: 2.2rem; font-weight: bold; letter-spacing: 2px; color: #333;">
+    {{-- <div style="position: absolute; top: 30px; left: 0; width: 100%; text-align: center; z-index: 10; font-size: 2.2rem; font-weight: bold; letter-spacing: 2px; color: #333;">
         轉盤抽籤 GDG 2025, 05, 21 DEMO
-    </div>
+    </div> --}}
     <div class="container">
         <div class="left-section">
             <div id="wheel-container">
